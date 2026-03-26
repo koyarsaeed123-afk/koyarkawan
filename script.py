b97d26452069b9aa10b292aa9224f71be868d1d5
koyarsaeed123-afk <koyarsaeed123@gmail.com>
2026-03-26 20:00:40 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,25 +1,29 @@
 import requests
 
+# ناونیشانی سەرچاوەکە
 url = "http://104.160.15.138/XRmDByf1QHTLYakHbDjPfjcc__ih__9PyRiGk8d42qmaDaz5I1k__ih__pfXjxavgua9Bp1zAx5XSBR4M3827ybujacXDxBeB__hi__uNa"
 
+# ئەم زانیارییانە وای لێ دەکات وەک مۆبایلەکەت دەربکەوێت
 headers = {
     "User-Agent": "And$MyTV/1.1 (Linux; Android 9; JKM-LX1 Build/HUAWEIJ KML-LX1)",
-    "Host": "104.160.15.138",
+    "Accept-Encoding": "gzip",
     "Connection": "Keep-Alive",
-    "X-Requested-With": "com.kurdish.tv"
+    "Host": "104.160.15.138"
 }
 
-def update():
+def get_link():
     try:
-        response = requests.get(url, headers=headers, timeout=20)
+        response = requests.get(url, headers=headers, timeout=15)
         if response.status_code == 200:
-            with open("playlist.m3u", "w", encoding="utf-8") as f:
-                f.write("#EXTM3U\n#EXTINF:-1,Falcon Eye\n" + response.text.strip())
-            print("Success!")
+            link = response.text.strip()
+            # دروستکردنی فایلی m3u
+            with open("falcon_eye.m3u", "w", encoding="utf-8") as f:
+                f.write("#EXTM3U\n#EXTINF:-1,Falcon Eye HD\n" + link)
+            print("Done! Link Updated.")
         else:
-            print(f"Error: {response.status_code}")
+            print(f"Server rejected us with code: {response.status_code}")
     except Exception as e:
-        print(f"Fail: {e}")
+        print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    update()
+    get_link()
